834a92a51b39ac9b5b25418661ebc167e91a58ee
Apple <apple@MacBook-Pro.local>
2025-04-10 12:18:31 +0300

    KYC . verification API

--- Accounts/api/serializer.py
+++ Accounts/api/serializer.py
@@ -4,42 +4,48 @@ from userAccount.models import User
 from Accounts.models import Account,KYC
 
 
-
-
-
-
-
 ############################################
 # KYC Serializer
 ###########################################
-
-
 class KYCSerializer(serializers.ModelSerializer):
     class Meta:
         model = KYC
         fields = [
-            # Personal Information
-            'full_name', 'Last_name', 'date_of_birth', 'gender',
-            # Identity Documents
-            'identity_type', 'identity_image',
-            # Verification Images
-            'profile_image', 'face_verification_image' 'biometric_hash',
-            # Address Information
-            'address_line1', 'address_line2', 'city', 'state', 'zip_code', 'country',
-            # Contact Details
-            'mobile', 'fax',
-            # Metadata (read-only)
+            'id',
+            'user',
+            'account',
+            'First_name',
+            'Last_name',
+            'date_of_birth',  # required field
+            'gender',
+            'identity_type',
+            'identity_image',
+            'profile_image',
+            'face_verification_image',
+            'biometric_hash',
+            'address_line1',
+            'address_line2',
+            'city',
+            'state',
+            'zip_code',
+            'country',
+            'mobile',
+            'fax',
             'date'
         ]
-        read_only_fields = ('biometric_hash', 'date')
+        read_only_fields = ("biometric_hash", "date")
+        extra_kwargs = {
+            'date_of_birth': {'required': True},
+            # ... any other fields you need to require
+        }
 
+        
+
+############################################
+# AccountSerializer
+###########################################
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account  # Specify the model to serialize
         fields = '__all__'  # This includes all fields from the Movie model
         
-class KycSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = KYC  # Specify the model to serialize
-        fields = '__all__'  # This includes all fields from the Movie model    
-       
